177103746bc4eceb3298b2b6884d313546138ac6
Mohamed-Sa2024 <mohammedsaeed@cleanlife.sa>
2024-12-07 09:09:48 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // const cors = require("cors");
 require("dotenv").config();
+const puppeteer = require('puppeteer-core');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,18 @@ app.use("/download", getpdfRouter);
 app.use("*", (req,res)=>{
   res.status(404).json("No content at this path")
 })
+
+
+(async () => {
+  const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome', // Path to Chrome binary
+    args: ['--no-sandbox', '--disable-setuid-sandbox'] // Required for many cloud environments
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await browser.close();
+})();
 
 
 
