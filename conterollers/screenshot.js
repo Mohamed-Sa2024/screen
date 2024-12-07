552cd5eecbd643a28b77e87410626039945d011e
Mohamed-Sa2024 <mohammedsaeed@cleanlife.sa>
2024-12-07 09:57:29 +0300

    Add files via upload

--- conterollers/screenshot.js
+++ conterollers/screenshot.js
@@ -16,7 +16,9 @@ const downloadImage = async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: ["--no-sandbox", "--disable-setuid-sandbox", '--disable-gpu',
+      '--disable-dev-shm-usage'],
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined
     });
 
     const page = await browser.newPage();
